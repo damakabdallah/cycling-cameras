a8792b7a32dab311c9111c52a3e6b78460e6e710
damakabdallah <xttrzax@gmail.com>
2019-05-12 18:42:43 +0200

    Initial commit

---
+++ Assets/cameras.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameras : MonoBehaviour {
+    public GameObject[] gameCameras;
+    int gameCameraIndex = 0;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+            changeCamera(1);
+        if (Input.GetMouseButtonDown(1))
+            changeCamera(-1);
+	}
+    void FocusOnCamera(int index)
+    {
+        for(int i = 0; i < gameCameras.Length; i++)
+        {
+            gameCameras[i].SetActive(i == index);
+        }
+    }
+    void changeCamera(int direction)
+    {
+        gameCameraIndex += direction;
+        if (gameCameraIndex >= gameCameras.Length)
+            gameCameraIndex = 0;
+        if (gameCameraIndex < 0)
+            gameCameraIndex = gameCameras.Length - 1;
+        FocusOnCamera(gameCameraIndex);
+    }
+}
---
+++ Assets/player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+
+
+public class player : MonoBehaviour {
+    public float speed = 3.5f;
+    public float jumpForce = 500f;
+    public float rotationSpeed = 40f;
+    public bool canJump = false;
+    public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += transform.forward * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.RotateAround(transform.position,Vector3.up,rotationSpeed*Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.RotateAround(transform.position, Vector3.up, -rotationSpeed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space)&& canJump)
+        {
+            
+            canJump = false;
+            rb.AddForce(0, jumpForce, 0);
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "floor")
+            canJump = true;
+    }
+}
